7f8ce6f5553bc7fe2a15608f73de63cd7985a77d
성진옥 <wlsdhr0831@naver.com>
2022-04-05 01:31:17 +0900

    feat: 로그인 tag 제작

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from "react";
+import Modal from "../../common/Modal";
 
 const SignIn = () => {
-    return <div><input/><input/></div>;
-}
+  const [errorMessage, setErrorMessage] = useState<string>("에러요");
+
+  return (
+    <Modal
+      buttonText="로그인"
+      onClick={() => {
+        alert("로그인");
+      }}
+      errorMessage={errorMessage}
+    >
+      <input placeholder="이메일" />
+      <input placeholder="비밀번호" />
+      <button
+        onClick={() => {
+          alert("비밀번호 찾기");
+        }}
+      >
+        비밀번호 찾기
+      </button>
+      아직 회원이 아니신가요?
+      <button
+        onClick={() => {
+          alert("회원가입 하기");
+        }}
+      >
+        회원가입 하기
+      </button>
+    </Modal>
+  );
+};
 
 export default SignIn;
