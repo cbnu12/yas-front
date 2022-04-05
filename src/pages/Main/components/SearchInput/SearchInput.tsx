c021b965690b3ffb0efd16c7aa84756ce74afb1b
성진옥 <wlsdhr0831@naver.com>
2022-04-06 00:42:30 +0900

    feat: 검색창 css 수정

--- src/pages/Main/components/SearchInput/SearchInput.tsx
+++ src/pages/Main/components/SearchInput/SearchInput.tsx
@@ -1,6 +1,8 @@
 import { CloseOutlined, SearchOutlined } from "@ant-design/icons";
 import { Select, Input, Tag, Typography } from "antd";
 
+import "./SearchInput.scss";
+
 interface Props {
   tagList: string[];
 }
@@ -9,17 +11,18 @@ const SearchInput = ({ tagList }: Props) => {
   const { Option } = Select;
 
   return (
-    <>
-      <Select defaultValue={"그룹명"}>
-        <Option>그룹명</Option>
-        <Option>해시태그</Option>
-      </Select>
-      <Input
-        placeholder="그룹명, 해시태그 검색"
-        size="large"
-        prefix={<SearchOutlined />}
-      />
-      <div>
+    <div className="main__search-input">
+      <div className="main__search-input__input-container">
+        <Select defaultValue={"그룹명"}>
+          <Option>그룹명</Option>
+          <Option>해시태그</Option>
+        </Select>
+        <div className="main__search-input__input-container__box">
+          <SearchOutlined className="search-icon" />
+          <input placeholder="그룹명, 해시태그 검색" />
+        </div>
+      </div>
+      <div className="main__search-input__tag-container">
         {tagList.map((item) => (
           <Tag key={item} color="default">
             <Typography.Text strong>#{item}</Typography.Text>
@@ -27,14 +30,14 @@ const SearchInput = ({ tagList }: Props) => {
         ))}
       </div>
 
-      <div>
+      {/* <div>
         {tagList.map((item) => (
           <Tag key={item} color="default" icon={<CloseOutlined />}>
             <Typography.Text strong>{item}</Typography.Text>
           </Tag>
         ))}
-      </div>
-    </>
+      </div> */}
+    </div>
   );
 };
 
