2129b1a4bce0c80ddd723efbbdf2515caad986fa
성진옥 <wlsdhr0831@naver.com>
2022-04-07 01:34:11 +0900

    feat: signUp 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import "antd/dist/antd.css";
 import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/signIn" element={<SignIn />} />
+      <Route path="/signUp" element={<SignUp />} />
     </Routes>
   );
 }
