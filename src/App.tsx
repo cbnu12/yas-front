cb67d86586e216876c52da3c3150fc98538ef736
성진옥 <wlsdhr0831@naver.com>
2022-04-13 00:03:23 +0900

    feat: 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import Main from "./pages/Main";
 import "antd/dist/antd.css";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import FindPassword from "./pages/FindPassword";
+import ConfirmEmail from "./pages/ConfirmEmail";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
       <Route path="/signIn" element={<SignIn />} />
       <Route path="/signUp" element={<SignUp />} />
+      <Route path="/findPassword" element={<FindPassword />} />
+      <Route path="/confirmEmail" element={<ConfirmEmail />} />
+      <Route path="/" element={<Main />} />
     </Routes>
   );
 }
