5978378d55020b493b6b56088640317ec01c4bb3
성진옥 <wlsdhr0831@naver.com>
2022-04-19 22:59:12 +0900

    feat: 로그인 진입 버튼 추가

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -5,10 +5,21 @@ import { MainListData, tagList } from "./data/data";
 import SortInput from "./components/SortInput";
 
 import "./Main.scss";
+import { useNavigate } from "react-router-dom";
+import { Button } from "antd";
 
 const Main = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="main">
+      <Button
+        onClick={() => {
+          navigate("/signIn");
+        }}
+      >
+        로그인 하기
+      </Button>
       <SearchInput tagList={tagList} />
       <SortInput />
       <MainView list={MainListData} />
