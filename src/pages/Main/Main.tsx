250e1aa46d1ad576ea95cb7aab5e35156020f3db
성진옥 <wlsdhr0831@naver.com>
2022-04-22 02:21:47 +0900

    feat: 메인 페이지 레이아웃 적용

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,29 +1,55 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useState, MouseEvent } from "react";
+import { Button, Layout } from "antd";
+import { MenuOutlined } from "@ant-design/icons";
 import SearchInput from "./components/SearchInput";
 import MainView from "./Main.view";
 import { MainListData, tagList } from "./data/data";
 import SortInput from "./components/SortInput";
 
 import "./Main.scss";
-import { useNavigate } from "react-router-dom";
-import { Button } from "antd";
+
+const { Sider, Content } = Layout;
 
 const Main = () => {
   const navigate = useNavigate();
+  const [showSider, setShowSider] = useState<boolean>(false);
 
   return (
-    <div className="main">
-      <Button
-        onClick={() => {
-          navigate("/signIn");
-        }}
-      >
-        로그인 하기
-      </Button>
-      <SearchInput tagList={tagList} />
-      <SortInput />
-      <MainView list={MainListData} />
-    </div>
+    <>
+      <Layout>
+        {showSider && (
+          <>
+            <div
+              className="main__sider__box"
+              onClick={() => setShowSider(false)}
+            />
+            <Sider className="main__sider" width="300">
+              <Button
+                className="main__sider__button"
+                shape="round"
+                onClick={() => {
+                  navigate("/signIn");
+                }}
+              >
+                로그인 하기
+              </Button>
+            </Sider>
+          </>
+        )}
+        <Content>
+          <div className="main">
+            <MenuOutlined
+              className="main__menu"
+              onClick={() => setShowSider(!showSider)}
+            />
+            <SearchInput tagList={tagList} />
+            <SortInput />
+            <MainView list={MainListData} />
+          </div>
+        </Content>
+      </Layout>
+    </>
   );
 };
 
