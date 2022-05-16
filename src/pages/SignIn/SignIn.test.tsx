5d41bb654808a3e7a0d6618aba74d30445a807b7
성진옥 <wlsdhr0831@naver.com>
2022-05-17 01:14:25 +0900

    test: add signIn test case

--- src/pages/SignIn/SignIn.test.tsx
+++ src/pages/SignIn/SignIn.test.tsx
@@ -14,15 +14,61 @@ describe("로그인 화면", () => {
     return { emailInput, passwordInput, submitButton };
   };
 
-  it("이메일과 비밀번호가 모두 입력되면 버튼이 활성화 된다.", () => {});
+  it("이메일과 비밀번호가 모두 입력되면 버튼이 활성화 된다.", () => {
+    const { emailInput, passwordInput, submitButton } = setUp();
 
-  it("이메일가 비밀번호가 조건에 충족되지 않으면 버튼이 비활성화 된다.", () => {});
+    userEvent.type(emailInput, "abc@naver.com");
+    userEvent.type(passwordInput, "1234");
+    expect(submitButton).toBeEnabled();
+  });
 
-  it("이메일 오류 시에 에러 메세지를 출력한다.", () => {});
+  it("이메일과 비밀번호가 조건에 충족되지 않으면 버튼이 비활성화 된다.", () => {
+    const { emailInput, passwordInput, submitButton } = setUp();
 
-  it("비밀번호 오류 시에 에러 메세지를 출력한다.", () => {});
+    userEvent.type(emailInput, "abc");
+    userEvent.type(passwordInput, "");
+    expect(submitButton).toNotBeEnabled();
+  });
 
-  it("5회 이상 실패일 경우 에러 메세지를 출력한다.", () => {});
+  it("이메일 오류 시에 에러 메세지를 출력한다.", () => {
+    const { emailInput, passwordInput, submitButton } = setUp();
 
-  it("정지된 계정일 경우 에러 메세지를 출력한다.", () => {});
+    userEvent.type(emailInput, "abc@naver.com");
+    userEvent.type(passwordInput, "1234");
+    userEvent.click(submitButton);
+
+    expect(screen.getByText(/이메일을 확인하세요./)).toBeInTheDocument();
+  });
+
+  it("비밀번호 오류 시에 에러 메세지를 출력한다.", () => {
+    const { emailInput, passwordInput, submitButton } = setUp();
+
+    userEvent.type(emailInput, "abc@naver.com");
+    userEvent.type(passwordInput, "1234");
+    userEvent.click(submitButton);
+
+    expect(screen.getByText(/비밀번호를 확인하세요./)).toBeInTheDocument();
+  });
+
+  it("5회 이상 실패일 경우 에러 메세지를 출력한다.", () => {
+    const { emailInput, passwordInput, submitButton } = setUp();
+
+    userEvent.type(emailInput, "abc@naver.com");
+    userEvent.type(passwordInput, "1234");
+    userEvent.click(submitButton);
+
+    expect(
+      screen.getByText(/비밀번호를 5회 이상 잘못 입력하셨습니다./)
+    ).toBeInTheDocument();
+  });
+
+  it("정지된 계정일 경우 에러 메세지를 출력한다.", () => {
+    const { emailInput, passwordInput, submitButton } = setUp();
+
+    userEvent.type(emailInput, "abc@naver.com");
+    userEvent.type(passwordInput, "1234");
+    userEvent.click(submitButton);
+
+    expect(screen.getByText(/정지된 계정입니다./)).toBeInTheDocument();
+  });
 });
