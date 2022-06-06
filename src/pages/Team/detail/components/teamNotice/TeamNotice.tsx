13087867cdae23408797e1cc0f62301ab5342724
성진옥 <wlsdhr0831@naver.com>
2022-06-06 23:25:44 +0900

    feat: 팀 공지사항 페이지 제작

--- src/pages/Team/detail/components/teamNotice/TeamNotice.tsx
+++ src/pages/Team/detail/components/teamNotice/TeamNotice.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import List from "../../../../../common/Board/List";
+import { ListItemProps } from "../../../../../common/Board/ListItem/ListItem";
+import Search from "../../../../../common/Search";
 
 const TeamNotice = () => {
-  return <>게시판</>;
+  const labelList: string[] = ["번호", "제목", "작성자", "작성일"];
+  const list: ListItemProps[] = [
+    {
+      index: 1,
+      title: "제목",
+      writer: "작성자",
+      createdDate: "2022.12.20",
+      commentCount: 10,
+    },
+    {
+      index: 2,
+      title: "제목",
+      writer: "작성자",
+      createdDate: "2022.12.20",
+    },
+    {
+      index: 3,
+      title: "제목",
+      writer: "작성자",
+      createdDate: "2022.12.20",
+      commentCount: 10,
+    },
+  ];
+
+  return (
+    <div>
+      <Search />
+      <List labelList={labelList} list={list} />
+    </div>
+  );
 };
 
 export default TeamNotice;
