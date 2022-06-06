7008eff5f09f2264cf8a2315199488786fdfe06a
성진옥 <wlsdhr0831@naver.com>
2022-06-07 00:54:02 +0900

    feat: 팀 정보 페이지 제작

--- src/pages/Team/detail/components/information/Information.tsx
+++ src/pages/Team/detail/components/information/Information.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+import "./Information.scss";
 
 const Information = () => {
-  return <>정보</>;
+  const { id } = useParams();
+
+  return <div className="team-information">정보</div>;
 };
 
 export default Information;
