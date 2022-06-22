169630ae6b64827617dff55914b31ff83aef0649
성진옥 <wlsdhr0831@naver.com>
2022-06-22 20:15:36 +0900

    feat: 팀 상세정보 채팅화면 제작

--- src/pages/Team/detail/components/teamChat/TeamChat.tsx
+++ src/pages/Team/detail/components/teamChat/TeamChat.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { chatList } from "../../../../Main/data/data";
+
+import "./TeamChat.scss";
 
 const TeamChat = () => {
-  return <>채팅</>;
+  const user = "A";
+
+  return (
+    <div className="chat-container">
+      {chatList.map((chat) => (
+        <div
+          className={`chat-container__wrapper ${
+            user === chat.writer ? "right" : "left"
+          }`}
+        >
+          <img
+            className="chat-container__wrapper__profile"
+            src={chat.imageSource}
+            alt="profile"
+          />
+          <div className="chat-container__wrapper__content">
+            <div className="chat-container__wrapper__content__message">
+              {chat.message}
+            </div>
+            <div className="chat-container__wrapper__content__time">
+              {chat.time}
+            </div>
+          </div>
+        </div>
+      ))}
+      <input className="chat-container__input" />
+    </div>
+  );
 };
 
 export default TeamChat;
